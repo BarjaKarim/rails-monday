0540ae2c0b8053cdf966f8cf795bdf3bd0399c9d
BarjaKarim <karim.barja@gmail.com>
2024-04-06 16:35:40 +0200

    show + date picker

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,27 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show]
 
   def index
     @cars = Car.all
   end
 
+  def show
+    @booking = Booking.new
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+private
+
+  def car_params
+    params.require(:car).permit(:model, :brand, :year, :km, :description, :price_per_day, :color, :photo)
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.destroy_all
 
 
 User.create!(first_name: "John", last_name: "Doe", email: "johndoe@gmail.com", password: "123456")
-Car.create!(model: "Ferrari", brand: "Tiguan", year: 1991, km:100000, color:"violet", description: "blabla", price_per_day: 100, photo: "https://images.unsplash.com/photo-1532635246-3f1e6f8d1b3f?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", user_id: User.first.id)
-Car.create!(model: "Porsche", brand: "Tiguan", year: 1991, km:100000, color:"violet", description: "blabla", price_per_day: 100, photo: "https://images.unsplash.com/photo-1532635246-3f1e6f8d1b3f?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", user_id: User.first.id)
-Car.create!(model: "Lamborghini", brand: "Tiguan", year: 1991, km:100000, color:"violet", description: "blabla", price_per_day: 100, photo: "https://images.unsplash.com/photo-1532635246-3f1e6f8d1b3f?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60", user_id: User.first.id)
+Car.create!(model: "Ferrari", brand: "Tiguan", year: 1991, km:100000, color:"violet", description: "blabla", price_per_day: 100, photo: "https://sf1.autoplus.fr/wp-content/uploads/autoplus/2021/01/ferrari-connaissez-vous-tous-les-rouges-marque-750x410.jpeg", user_id: User.first.id)
+Car.create!(model: "Porsche", brand: "Tiguan", year: 1991, km:100000, color:"violet", description: "blabla", price_per_day: 100, photo: "https://sf1.autoplus.fr/wp-content/uploads/autoplus/2021/01/ferrari-connaissez-vous-tous-les-rouges-marque-750x410.jpeg", user_id: User.first.id)
+Car.create!(model: "Lamborghini", brand: "Tiguan", year: 1991, km:100000, color:"violet", description: "blabla", price_per_day: 100, photo: "https://sf1.autoplus.fr/wp-content/uploads/autoplus/2021/01/ferrari-connaissez-vous-tous-les-rouges-marque-750x410.jpeg", user_id: User.first.id)
 puts "....."
